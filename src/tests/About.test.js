6d7e2cef32e3d200b3422649a9ba06a89cbb21b3
Nícolas <nicolasbrga01@gmail.com>
2023-01-20 16:24:39 -0300

    Requisito 4 aprovado pelo stryker

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import { act } from 'react-dom/test-utils';
 import { About } from '../pages';
 import renderWithRouter from '../renderWithRouter';
 
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 4', () => {
+  it('Testa se contém o h2', () => {
+    renderWithRouter(<NotFound />);
+
+    const title = screen.getByRole('heading', { name: 'Page requested not found' });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Testa se contém a imagem correta', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByRole('img', { name: 'Pikachu crying because the page requested was not found' });
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
